bd0ac0380b16bb302495d6024bf2d45aa563f780
Omid Alikhtl <omidrazi_1989@yahoo.com>
2023-05-10 17:28:03 -0700

    second cimmit

--- app.js
+++ app.js
@@ -17,3 +17,4 @@ foo();
 // but after implementing event loop task number 1 3 2  as task 2 has 2 sec delay.
 //  keeping track of function in call stack and removing the one done is called event loop
 //  call stack is the call where the order of functions exist.
+// adding this commit
